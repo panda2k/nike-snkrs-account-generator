b52e852c89dbf4cb4ce2d686b31692a881027a78
panda2k <thelowkeypanda@gmail.com>
2019-08-08 09:27:02 -0700

    Run the Gmail Gen in App.java
    Incomplete write to file implemented

--- src/main/java/nikesnkrsaccountgen/App.java
+++ src/main/java/nikesnkrsaccountgen/App.java
@@ -1,9 +1,22 @@
 package nikesnkrsaccountgen;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class App {
     public static void main( String[] args ) {
         System.setProperty("webdriver.gecko.driver","geckodriver.exe");
+        //String fileName = "accounts_" + 
         GmailGen gen = new GmailGen();
-        gen.getPhoneNumber();
+        try {
+            PrintWriter textWriter = new PrintWriter(new File("accounts.txt"));
+            String[][] emailArray = gen.generateGmail();
+            textWriter.append(emailArray[0][0]);
+            textWriter.append(emailArray[0][1]);
+        } 
+        catch (FileNotFoundException e) {
+            System.out.println("accounts.txt file not found");
+        }
     }
 }
