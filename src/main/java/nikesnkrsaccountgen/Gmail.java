f2420e53ac09f81a8fe94940e41aa8783b0c385b
panda2k <thelowkeypanda@gmail.com>
2019-08-13 17:28:18 -0700

    Remove extra Date Dependencies

--- src/main/java/nikesnkrsaccountgen/Gmail.java
+++ src/main/java/nikesnkrsaccountgen/Gmail.java
@@ -1,6 +1,5 @@
 package nikesnkrsaccountgen;
 
-import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class Gmail {
--- src/main/java/nikesnkrsaccountgen/GmailGen.java
+++ src/main/java/nikesnkrsaccountgen/GmailGen.java
@@ -3,7 +3,6 @@ package nikesnkrsaccountgen;
 import java.util.Random;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 import java.io.File;
 import java.io.FileNotFoundException;
