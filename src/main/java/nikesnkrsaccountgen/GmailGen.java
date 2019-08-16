2b123476c9a4fc22dc16ffc50de771823402b5e2
panda2k <thelowkeypanda@gmail.com>
2019-08-16 15:35:42 -0700

    Update the GmailGen to extend the AccountGenerator class

--- src/main/java/nikesnkrsaccountgen/GmailGen.java
+++ src/main/java/nikesnkrsaccountgen/GmailGen.java
@@ -6,37 +6,15 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Scanner;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.*;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 
-public class GmailGen {
+public class GmailGen extends AccountGenerator{
     WebDriver driver;
-    CloseableHttpClient requestsClient = HttpClients.createDefault();
-    ResponseHandler errorHandler = new ResponseHandler<String>() {
-        public String handleResponse(HttpResponse response) throws IOException{
-            if(response.getStatusLine().getStatusCode() == 200) {
-                return EntityUtils.toString(response.getEntity());
-            }
-            else {
-                return "failed";
-            }
-        }
-    };
-
-    final String getSMSCodeAPIURL = "http://api.getsmscode.com/vndo.php";
-    final String username = "littlwang@gmail.com";
-    final String token = "36d0674c163d6b68ae2ccc89e461ed1f";
     final int startingTypeSpeed = 75;
 
     private Gmail newGmail;
@@ -95,8 +73,9 @@ public class GmailGen {
         System.out.println("Verification code:" + verificationCode);
         
         waiter.until(ExpectedConditions.elementToBeClickable(By.id("code"))); 
-        typeKeys(phoneNumber, randomGen.nextInt(11) + startingTypeSpeed, driver.findElement(By.id("code"))); 
+        typeKeys(verificationCode, randomGen.nextInt(11) + startingTypeSpeed, driver.findElement(By.id("code"))); 
         driver.findElement(By.id("gradsIdvVerifyNext")).click();
+        blacklistNumber(phoneNumber);
 
         waiter.until(ExpectedConditions.elementToBeClickable(By.id("month"))); 
         enterRandomInfo();
@@ -122,61 +101,7 @@ public class GmailGen {
         System.out.println("Finished created google account.\nEmail is " + newGmail.getEmailAddress() + "\nPassword is: " + newGmail.getPassword());
         return newGmail;
     }
-
-    private String getPhoneNumber() {
-        String getNumberURL = getSMSCodeAPIURL + "?action=getmobile&" + "username=" + username + "&token=" + token + "&pid=1" + "&cocode=uk"; 
-        String phoneNumber = "+";
-        HttpPost getSMSRequest = new HttpPost(getNumberURL);
-        getSMSRequest.addHeader("Accept", "application/json");
-        getSMSRequest.addHeader("Content-type", "application/json");
-
-        System.out.println("Now executing API request " + getSMSRequest.getRequestLine());
-
-        try {
-            String response = requestsClient.execute(getSMSRequest, errorHandler);
-            System.out.println("response: " + response);
-            phoneNumber += response;
-        } 
-        catch (IOException e) {
-            System.out.println("IOException. Please check URL parameters");
-        }
-            
-        return phoneNumber;
-    }
-
-    private String getMessage(String phoneNumber) {
-        String message = "Message|Not Receive"; 
-        String getMessageURL = getSMSCodeAPIURL + "?action=getsms&username=" + username + "&token=" + token + "&pid=1&mobile=" + phoneNumber + "&cocode=uk"; 
-        HttpPost getSMSRequest = new HttpPost(getMessageURL);
-        getSMSRequest.addHeader("Accept", "application/json");
-        getSMSRequest.addHeader("Content-type", "application/json");
-
-        System.out.println("Now executing API request " + getSMSRequest.getRequestLine());
-        do {
-            try {
-                String response = requestsClient.execute(getSMSRequest, errorHandler);
-                System.out.println("response: " + response);
-                message = response;
-            } 
-            catch (IOException e) {
-                System.out.println("IOException. Please check URL parameters");
-            }
     
-            if(message.equals("Message|Not Receive") == true) {
-                try {
-                    Thread.sleep(10000);
-                    System.out.println("No message recieved, sleeping for 10 seconds");                    
-                } 
-                catch (InterruptedException e) {
-                    System.out.println("Unable to pause thread");
-                }
-            }
-            
-        } while(message.equals("Message|Not Receive") == true);
-        System.out.println("Receieved message\nMessage:'" + message + "'");
-        return message;
-    }
-
     private String randomNumberString(int digits) {
         Random randomGen = new Random();
         String randomNumbers = "";
@@ -186,17 +111,6 @@ public class GmailGen {
         }
 
         return randomNumbers;
-    }
-
-    private String randomPassword(int characterCount) {
-        String password = "";
-        Random randomGen = new Random();
-
-        for(int count = 0; count < characterCount; count++) {
-            password += (char) randomGen.nextInt(75) + 48;
-        }
-
-        return password;
     }
 
     private String getFirstName() {
@@ -275,17 +189,5 @@ public class GmailGen {
         gender.selectByValue(Integer.toString(newGmail.getGender()));
 
         newGmail.setDateOfBirth(birthday);
-    }
-
-    private void typeKeys(String input, int wordsPerMinute, WebElement element) {
-        for(int count = 0; count < input.length(); count++) {
-            element.sendKeys(Character.toString(input.charAt(count)));
-            try {
-                Thread.sleep((int) (1000 / ((double) wordsPerMinute / 60)) / 5); // divide by 5 to account for average word length
-            } 
-            catch (InterruptedException e) {
-                System.out.println("Error when trying to pause keystrokes.");
-            }
-        }
     }
 }
