d0004e424bbccf27446c990cfaaad98b3557b945
panda2k <thelowkeypanda@gmail.com>
2019-08-18 12:48:47 -0700

    Version 1 of Protonmail Account Generator
    Fully functional in testing so far.

--- src/main/java/nikesnkrsaccountgen/ProtonmailGenerator.java
+++ src/main/java/nikesnkrsaccountgen/ProtonmailGenerator.java
@@ -1,8 +1,12 @@
 package nikesnkrsaccountgen;
 
+import java.util.ArrayList;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -20,18 +24,85 @@ public class ProtonmailGenerator extends AccountGenerator{
         String lastName = getLastName();
         String emailAddress = firstName + lastName.charAt(0) + randomNumberString(10);
         String password = randomPassword(10);
-        String phoneNumber;
-
+        String phoneNumber = getPhoneNumber("cn", "404");
+        Actions builder = new Actions(driver);
+        String verificationMessage;
+        String verificationCode = "";
+        
         driver.get("https://mail.protonmail.com/create/new?language=en");
-        typeKeys(emailAddress, 75, driver.findElement(By.id("username")));
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            System.out.println("Caught exception while sleeping");
+        }
+        System.out.println("Done sleeping");
+
+        Actions typeUsername = builder.sendKeys(
+        Keys.chord(Keys.TAB) +
+        Keys.chord(Keys.TAB) +
+        Keys.chord(Keys.TAB) +
+        Keys.chord(Keys.TAB));
+        typeUsername.perform();
+        browserTypeKeys(builder, emailAddress, 75);
+        try {
+            Thread.sleep(1500);
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+        if(driver.getPageSource().contains("Username already used")) {
+            Actions fixEmail = builder.sendKeys(Keys.BACK_SPACE);
+            fixEmail.perform();
+        }
+
         typeKeys(password, 75, driver.findElement(By.id("password")));
         typeKeys(password, 75, driver.findElement(By.id("passwordc")));
-        driver.findElement(By.name("submitBtn")).click();
+        
+        Actions enterAccount = builder.sendKeys(
+        Keys.chord(Keys.TAB) + 
+        Keys.chord(Keys.TAB) +
+        Keys.chord(Keys.TAB) +
+        Keys.chord(Keys.TAB) +
+        Keys.chord(Keys.TAB) +
+        Keys.chord(Keys.ENTER));
+        enterAccount.perform();
+
         waiter.until(ExpectedConditions.elementToBeClickable(By.id("confirmModalBtn")));
         driver.findElement(By.id("confirmModalBtn")).click();
-        waiter.until(ExpectedConditions.elementToBeClickable(By.id("id-signup-radio-sms")));
-        driver.findElement(By.id("id-signup-radio-sms")).click();
+        waiter.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='humanVerification-block-sms']//label[@class='signup-radio-label']"))).click();
+        typeKeys(phoneNumber, 75, driver.findElement(By.id("smsVerification")));
+        driver.findElement(By.xpath("//button[@class='pm_button primary codeVerificator-btn-send']")).click();
 
+        verificationMessage = getMessage(phoneNumber.substring(1), "cn", "404");
+        if(verificationMessage.equals("no message recieved")) {
+            generateAccount();
+        }
+        for(int count = 1; count < verificationMessage.length(); count++) { // start count at 2 to get out of starting message
+            if(Character.isDigit(verificationMessage.charAt(count))) {
+                verificationCode += verificationMessage.charAt(count);
+            }
+        }
+
+        System.out.println("code is: " + verificationCode);
+        driver.findElement(By.id("codeValue")).sendKeys(verificationCode);
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+        newEmail = new Email(emailAddress, password);
         return newEmail;
+    }
+
+    private void browserTypeKeys(Actions builder, String input, int wordsPerMinute) {
+        Actions typeAction;
+        for(int count = 0; count < input.length(); count++) {
+            typeAction = builder.sendKeys(Character.toString(input.charAt(count)));
+            typeAction.perform();
+            try {
+                Thread.sleep((int) (1000 / ((double) wordsPerMinute / 60)) / 5); // divide by 5 to account for average word length
+            } 
+            catch (InterruptedException e) {
+                System.out.println("Error when trying to pause keystrokes.");
+            }
+        }
     }
 }
